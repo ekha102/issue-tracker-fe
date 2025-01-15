1f62fed25e72e3facfba280a1e03f57f7e6322fb
Eric Kha <erkha102@gmail.com>
2025-01-14 23:29:29 -0800

    Added the edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,37 +1,53 @@
 import { prisma } from '@/prisma/client'
-import { Flex, Heading, Text } from '@radix-ui/themes';
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import React from 'react'
 import IssueStatusBudge from '../issueStatusBudge';
+import Markdown from 'react-markdown';
+import { Pencil2Icon } from "@radix-ui/react-icons"
+import Link from 'next/link';
 
 interface Props {
-  params: {id: string}
+  params: { id: string }
 }
 
 
-const issueDetailsPage = async ({params}: Props) => {
+const issueDetailsPage = async ({ params }: Props) => {
   if (isNaN(Number(params.id))) notFound();
-  
- 
+
+
 
   const issueDetails = await prisma.issue.findUnique({
-    where: {issue_id: parseInt(params.id)},
+    where: { issue_id: parseInt(params.id) },
   });
-  
+
   if (!issueDetails) notFound();
-  
-  
-  const {issue_title, issue_desc, issue_status, issue_created} = issueDetails;
+
+
+  const { issue_id, issue_title, issue_desc, issue_status, issue_created } = issueDetails;
 
 
   return (
     <div>
       <h1>Issue Details:</h1>
-      <Heading as="h1">{issue_title}</Heading>
-      <Flex className='gap-2'>
-        <IssueStatusBudge status={issue_status} />
-        <Text>{issue_created.toDateString()}</Text>
-      </Flex>
+      <Grid columns={{initial:"1", md:"2"}} gap="8">
+        <Box>
+          <Heading as="h1">{issue_title}</Heading>
+          <Flex className='gap-2'>
+            <IssueStatusBudge status={issue_status} />
+            <Text>{issue_created.toDateString()}</Text>
+          </Flex>
+          <Card>
+            <Markdown>{issue_desc}</Markdown>
+          </Card>
+        </Box>
+
+        <Box>
+          <Button><Pencil2Icon/> <Link href={`/issues/edit/${issue_id}`}>Edit Issue </Link></Button>
+        </Box>
+
+      </Grid>
+
     </div>
   )
 }
