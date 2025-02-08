9f55df266e1f48e31f6afde08af0962327437c56
Eric Kha <erkha102@gmail.com>
2025-02-08 00:42:20 -0800

    Built assignee select component

--- app/issues/_components/SelectAssignee.tsx
+++ app/issues/_components/SelectAssignee.tsx
@@ -1,16 +1,27 @@
 "use client"
 import { Select } from '@radix-ui/themes'
 import React from 'react'
+import useSWR from 'swr'
 
+const fetcher = (url: string) => fetch(url).then(res => res.json())
 const SelectAssignee = () => {
+
+  const {data:users=[], error, isLoading} = useSWR("/api/users", fetcher);
+
+  console.log("Users: ", users)
+
+
   return (
     <Select.Root defaultValue="User">
       <Select.Trigger />
       <Select.Content>
         <Select.Group>
           <Select.Label>User</Select.Label>
-          <Select.Item value="1">Eric Kha</Select.Item>
-          <Select.Item value="2">Tim Kha</Select.Item>
+          {users?.map((user) => {
+            return (
+              <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
+            );
+          })}
         </Select.Group>
       </Select.Content>
     </Select.Root>
